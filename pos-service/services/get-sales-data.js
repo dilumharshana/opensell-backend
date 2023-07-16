6534096ad808bffd82e1f8193d219365fc756afb
dilumharshana <dilum.harshana23@gmail.com>
2023-07-16 20:23:16 +0530

    added new changes

--- pos-service/services/get-sales-data.js
+++ pos-service/services/get-sales-data.js
@@ -2,45 +2,36 @@ import { connection } from "../../utility-service/index.js";
 import { saleQueries } from "../constants/db-queries.js";
 
 export const get_sales_data = async (req, res) => {
-  const response = await connection.selectExecute(saleQueries.getSalesData, []);
+  const saleId = req?.query?.saleId || null;
+
+  //ifsinle sale request
+  if (saleId) {
+    const response = await connection.selectExecute(
+      saleQueries.getSingleSaleData,
+      {
+        1: saleId,
+      }
+    );
+
+    if (response?.rows) {
+      const saleData = await prepareSaleData(response?.rows);
+
+      return res.json({
+        status: 200,
+        data: saleData?.[saleId],
+      });
+    }
+
+    return res.json({
+      status: 500,
+      data: "Faild to load sales !",
+    });
+  }
+
+  const response = await connection.selectExecute(saleQueries.getSalesData, {});
 
   if (response?.rows) {
-    const saleData = {};
-
-    response?.rows?.forEach((sale) => {
-      const targetSale = sale?.SALE_ID;
-
-      if (saleData.hasOwnProperty(targetSale)) {
-        //adding selled items in to a existing sale in saleData object
-        const salesItemsArray = saleData[targetSale].ITEMS;
-
-        saleData[targetSale].ITEMS[salesItemsArray.length] = {
-          ITEM_NAME: sale?.ITEM_NAME,
-          QUANTITY: sale?.QUANTITY,
-          ITEM_CODE: sale?.ITEM_CODE,
-        };
-
-        return;
-      }
-
-      const saleDataObject = {
-        SALE_ID: targetSale,
-        RECIVED_CASH: sale?.RECIVED_CASH,
-        DISCOUNT: sale?.DISCOUNT,
-        SALE_DATE: sale?.SALE_DATE,
-        SELLED_BY: sale?.SELLED_BY,
-        ITEMS: [
-          {
-            ITEM_NAME: sale?.ITEM_NAME,
-            QUANTITY: sale?.QUANTITY,
-            ITEM_CODE: sale?.ITEM_CODE,
-          },
-        ],
-      };
-
-      //appending first item in to sales data object
-      saleData[sale?.SALE_ID] = saleDataObject;
-    });
+    const saleData = await prepareSaleData(response?.rows);
 
     return res.json({
       status: 200,
@@ -52,4 +43,49 @@ export const get_sales_data = async (req, res) => {
     status: 500,
     data: "Faild to load sales !",
   });
+};
+
+const prepareSaleData = (data) => {
+  const saleData = {};
+
+  data?.forEach((sale) => {
+    const targetSale = sale?.SALE_ID;
+
+    if (saleData.hasOwnProperty(targetSale)) {
+      //adding selled items in to a existing sale in saleData object
+      const salesItemsArray = saleData[targetSale].ITEMS;
+
+      saleData[targetSale].ITEMS[salesItemsArray.length] = {
+        ITEM_NAME: sale?.ITEM_NAME,
+        QUANTITY: sale?.QUANTITY,
+        ITEM_CODE: sale?.ITEM_CODE,
+        SELLED_PRICE: sale?.SELLED_PRICE,
+        SALE_DATE:sale?.SALE_DATE
+      };
+
+      return;
+    }
+
+    const saleDataObject = {
+      SALE_ID: targetSale,
+      RECIVED_CASH: sale?.RECIVED_CASH,
+      DISCOUNT: sale?.DISCOUNT,
+      SALE_DATE: sale?.SALE_DATE,
+      SELLED_BY: sale?.SELLED_BY,
+      ITEMS: [
+        {
+          ITEM_NAME: sale?.ITEM_NAME,
+          QUANTITY: sale?.QUANTITY,
+          ITEM_CODE: sale?.ITEM_CODE,
+          SELLED_PRICE: sale?.SELLED_PRICE,
+        },
+      ],
+    };
+
+    //appending first item in to sales data object
+    saleData[sale?.SALE_ID] = saleDataObject;
+
+  });
+  
+  return saleData
 };
